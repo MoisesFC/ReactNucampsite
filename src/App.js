d67d5e9b7b5a6f30fef790c6930a018205531ec3
Moises Figueroa <figueroa.abe@outlook.com>
2023-03-23 01:08:32 -0400

    added campsite selector

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import CampsitesList from './features/campsites/CampsitesList.js';
+import CampsitedDirectoryPage from './pages/CampsitesDirectoryPage';
 import Header from './components/Header.js';
 import Footer from './components/Footer';
+import './App.css';
 
 import './App.css';
 
@@ -9,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <CampsitesList/>
+      <CampsitedDirectoryPage/>
       <Footer/>
     </div>
   );
--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,11 +1,13 @@
-import { CAMPSITES } from '../../app/shared/CAMPSITES';
 import {Col, Row} from 'reactstrap';
 import CampsiteCard from "./CampsiteCard.js";
+import { selectAllCampsites } from './campsitesSlice';
 
 const CampsitesList = () => {
+    const campsites = selectAllCampsites();
+
     return(
         <Row className="ms-auto">
-            {CAMPSITES.map((campsite) =>{
+            {campsites.map((campsite) =>{
                 return (
                     <Col md='5' className='m-4' key={campsite.id}>
                         <CampsiteCard campsite={campsite}/>
